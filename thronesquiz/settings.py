0afec82dfac2ed5e09ded7640c58a1b8a4c68b5c
Weston Beard <weston.beard@gmail.com>
2015-10-02 14:39:09 -0400

    adding pg db

--- thronesquiz/settings.py
+++ thronesquiz/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'thronesquiz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'quizofthronesdjdb',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'thereisnopassword',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Internationalization
