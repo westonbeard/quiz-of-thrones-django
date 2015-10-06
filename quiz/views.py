e7e776802c1503130740c15b7b7a6160e40708ff
Weston Beard <weston.beard@gmail.com>
2015-10-06 17:28:21 -0400

    data is rendering

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
+from quiz.models import Question, Answer
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'quiz/index.html'
+
+
+    def get_queryset(self):
+        return Question.objects.all()
