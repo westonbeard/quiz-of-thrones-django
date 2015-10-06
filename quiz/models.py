ae8f46f282a8f5310d134c0f89b43a436fdf9cb7
Weston Beard <weston.beard@gmail.com>
2015-10-06 10:58:04 -0400

    changing question_id to question in Answer model. also migrated that change

--- quiz/models.py
+++ quiz/models.py
@@ -5,7 +5,14 @@ class Question(models.Model):
     content = models.CharField(max_length=300)
 
 
+    def __str__(self):
+        return self.content
+
+
 class Answer(models.Model):
     content = models.CharField(max_length=300)
     correct = models.BooleanField()
-    question_id = models.ForeignKey(Question)
+    question = models.ForeignKey(Question)
+
+    def __str__(self):
+        return self.content
