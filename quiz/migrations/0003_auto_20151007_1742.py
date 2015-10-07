1fac9bd74ce342887f0909a29fba7670073daed0
Weston Beard <weston.beard@gmail.com>
2015-10-07 13:43:18 -0400

    adding user, score models

---
+++ quiz/migrations/0003_auto_20151007_1742.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('quiz', '0002_auto_20151006_1435'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Score',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('correct', models.IntegerField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=25)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='score',
+            name='user',
+            field=models.ForeignKey(to='quiz.User'),
+        ),
+    ]
--- quiz/models.py
+++ quiz/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.db.models import IntegerField
+
 
 class Question(models.Model):
     content = models.CharField(max_length=300)
@@ -13,6 +15,22 @@ class Answer(models.Model):
     content = models.CharField(max_length=300)
     correct = models.BooleanField()
     question = models.ForeignKey(Question)
+
+    def __str__(self):
+        return self.content
+
+
+class User(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.content
+
+
+
+class Score(models.Model):
+    correct = IntegerField()
+    user = models.ForeignKey(User)
 
     def __str__(self):
         return self.content
