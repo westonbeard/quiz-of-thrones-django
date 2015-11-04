ffcbc5f2bc095b0fbfd5926c4e0434adc55c48b9
wbeard <wbeard@mbopartners.com>
2015-11-04 09:11:35 -0500

    adding more scores logic to views

--- quiz/views.py
+++ quiz/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.views.generic import TemplateView
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, request
 from django.template import RequestContext, loader
 
 from quiz.models import Question, Answer
@@ -28,13 +28,16 @@ class ScoresView(TemplateView):
         context['answers'] = Answer.objects.all()
         return context
 
-    def get_score(request, question_id):
+    @property
+    def get_score(request):
         num_questions = 3
         correct = 0
-        # selected_answer = Question.objects.all().answer_set.get(pk=request.POST['choice'])
-        for question_id, answer_pk in selected_answer:
+        selected_answer_id = Question.objects.all().answer_set.get(pk=request.POST['answer.pk'])
+        print selected_answer_id
+        # return 0 if not request.POST['answer.pk']
+        for question_id,answer_pk in request.POST['answer.pk']:
             setting_question = Question.objects.get(question_id)
-            correct_answer_id = setting_question.answer_set.all().filter(correct=True).first().pk
-            if correct_answer_id = selected_answer_id:
+            correct_answer_id = setting_question.answer_set.filter(correct=True).first().pk
+            if correct_answer_id == int(selected_answer_id):
                 correct += 1
             return correct/num_questions
